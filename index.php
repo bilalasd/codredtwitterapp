776f2fb0b1acc51900815c880aa5be4a1852b2d5
bilalasd <bilal2206@gmail.com>
2015-11-22 03:21:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$data = json_decodefile_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=1'));
+$data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=1'));
 
 print $data->gender;
 ?>
