d3dc3d2e99f2c1b0ca6387523285b7cd55d5e7e8
bilalasd <bilal2206@gmail.com>
2015-11-22 07:27:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
-$data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=1000'));
+$data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=10'));
 
+
+//for each tweet, get data
 foreach ($data->tweets as $value){
-  echo $value->cde->author->location->state;
+  $city = $value->cde->author->location->city;
+  $state = $value->cde->author->location->state;
+  echo city;
+  echo " ";
+  echo state;
   echo "<br>";
 }
+//----
+
 //echo $data->tweets[0]->cde->author->gender;
 ?>
