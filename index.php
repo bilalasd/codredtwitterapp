25f4c07c180bcbe8d734324f2c2b8771937b8b5e
bilalasd <bilal2206@gmail.com>
2015-11-22 08:07:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,17 +9,17 @@ foreach ($data->tweets as $value){
   $country = $value->cde->author->location->country;
  // if(country == 'United States'){
     if(!empty($state) || !empty($city)){
-      echo "city".$city;
-      echo " ";
-      echo "state".$state;
-      echo " <br>";
+      //echo "city".$city;
+      //echo " ";
+      //echo "state".$state;
+      //echo " <br>";
       
       
       //geocode
       $geocodedData = json_decode(file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address=houston,tx&key=AIzaSyAg2T1zc4lXqSYzOIv2AgYMzKu0w80wmTI'));
       $lat = $geocodedData->results[0]->geometry->location->lat;
       $lng = $geocodedData->results[0]->geometry->location->lng;
-      
+      echo $lat." ".$lng;
     }
  // }
 }
