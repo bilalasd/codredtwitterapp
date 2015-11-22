0a7535f6489fd670848035848b025c4b66bf9030
bilalasd <bilal2206@gmail.com>
2015-11-22 12:44:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
 </html>
 <?php
 
-$data = json_decode(file_get_contents('https://93b18000-37fe-479a-ac00-1a163f963bf7:lVZJbq2xyT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23'.$hashtagparam.'&size='.$numberparam));
+$data = json_decode(file_get_contents('https://93b18000-37fe-479a-ac00-1a163f963bf7:lVZJbq2xyT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23nyc&size=10));
 
 
 $latArr = array();
