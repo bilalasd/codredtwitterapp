12a62e231269b217952b0a83449f74989023ce90
bilalasd <bilal2206@gmail.com>
2015-11-22 03:42:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=10'));
 
 foreach ($data->tweets as $value){
-  echo $value->cde->author->gender;
+  echo $value->cde->author->location->state;
+  echo <br>;
 }
 //echo $data->tweets[0]->cde->author->gender;
 ?>
