6d9281bf8b841e27a4478b926354d5877adfc62d
bilalasd <bilal2206@gmail.com>
2015-11-21 23:25:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-echo "<h2>Simple Twitter API Test</h2>";
+require_once('TwitterAPIExchange.php');
+ 
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "YOUR_OAUTH_ACCESS_TOKEN",
+    'oauth_access_token_secret' => "YOUR_OAUTH_ACCESS_TOKEN_SECRET",
+    'consumer_key' => "YOUR_CONSUMER_KEY",
+    'consumer_secret' => "YOUR_CONSUMER_SECRET"
+);
+ 
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+ 
+$requestMethod = "GET";
+ 
+$getfield = '?screen_name=iagdotme&count=20';
+ 
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest();
 ?>
