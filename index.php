b05d8d3829d27af7c0d425d8e21ce39d91fe8940
bilalasd <bilal2206@gmail.com>
2015-11-22 11:11:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ function getPoints() {
   {
     echo "new google.maps.LatLng(".$latArr[$i].",".$lngArr[$i]."),";
   }
-   echo "new google.maps.LatLng(".$latArr[count($latArr)-1].",".$lngArr[count($lngArr)-1]);
+   echo "new google.maps.LatLng(".$latArr[count($latArr)-1].",".$lngArr[count($lngArr)-1].")";
    ?>];
 }
 
