c5af10b52cbd0c64eb7f93c9da567326891cfb00
bilalasd <bilal2206@gmail.com>
2015-11-22 10:21:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -44,6 +44,8 @@
 $data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=10'));
 
 
+$lat = array();
+$lng = array();
 
 $counter = 0;
 foreach ($data->tweets as $value){
