1a6d807e7e066154d6c1cf76002af0e27934a13e
bilalasd <bilal2206@gmail.com>
2015-11-22 12:52:36 -0600

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ foreach ($data->tweets as $value){
       
       
 
-      $geocodedData = json_decode(file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address='.$city.','.'$state.&key=AIzaSyAg2T1zc4lXqSYzOIv2AgYMzKu0w80wmTI"));
+      $geocodedData = json_decode(file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address='.$city.','.'$state.&key=AIzaSyChRFN775yqDhIAPc4_lG9ivzFtRP-e-wI"));
       $lat = $geocodedData->results[0]->geometry->location->lat;
       $lng = $geocodedData->results[0]->geometry->location->lng;
       $latArr[$counter] = $lat;
