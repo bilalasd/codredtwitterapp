c84c6181690e256202d744c33a1a6f815d1c0d3d
bilalasd <bilal2206@gmail.com>
2015-11-22 02:57:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$json = file_get_contents('https://cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=10');
+$json = file_get_contents("https://cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=10");
 $obj = json_decode($json);
-echo $obj->author;
+echo $obj;
 ?>
