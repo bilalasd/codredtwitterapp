4c5826e0d8b61fda3c2a277d971269fc02a5ee5f
bilalasd <bilal2206@gmail.com>
2015-11-22 08:08:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ foreach ($data->tweets as $value){
       $geocodedData = json_decode(file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address=houston,tx&key=AIzaSyAg2T1zc4lXqSYzOIv2AgYMzKu0w80wmTI'));
       $lat = $geocodedData->results[0]->geometry->location->lat;
       $lng = $geocodedData->results[0]->geometry->location->lng;
-      echo $lat." ".$lng;
+      echo $lat." ".$lng."<br>";
     }
  // }
 }
