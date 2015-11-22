b45e725a3bc2633bdce2f1336ec4191441df343c
bilalasd <bilal2206@gmail.com>
2015-11-22 11:29:00 -0600

    Update index.php

--- index.php
+++ index.php
@@ -147,11 +147,12 @@ function changeOpacity() {
 function getPoints() {
   var returnArr;
 
-  return [ <?php
-  $counter = 0;
-  foreach($lngArr as $lngVal){
-      echo "new google.maps.LatLng(".$latArr[$i].",".$lngVal."),";
+  return [ <?php for($i=0;$i<count($lngArr)-1;$i++)
+  {
+    echo "new google.maps.LatLng(".$latArr[$i].",".$lngArr[$i]."),";
+    echo $i;
   }
+   echo "new google.maps.LatLng(".$latArr[count($latArr)-1].",".$lngArr[count($lngArr)-1].")";
    ?>];
 }
 
