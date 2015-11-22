950ceb18b69a69a9cc1921e3c46c5f670a28bbcb
bilalasd <bilal2206@gmail.com>
2015-11-22 12:50:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <html>
   <head>
     
-    <INPUT TYPE = "Text" VALUE ="text" NAME = "hashtag">
-    <INPUT TYPE = "Text" VALUE ="text" NAME = "number">
+
 
     
     
@@ -55,7 +54,7 @@
 </html>
 <?php
 
-$data = json_decode(file_get_contents("https://93b18000-37fe-479a-ac00-1a163f963bf7:lVZJbq2xyT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23nyc&size=10"));
+$data = json_decode(file_get_contents('https://93b18000-37fe-479a-ac00-1a163f963bf7:lVZJbq2xyT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23'.$hashtagparam.'&size='.$numberparam));
 
 
 $latArr = array();
