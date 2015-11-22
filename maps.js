be712bcb065a725149788bffdc48e51e20ef0e9a
bilalasd <bilal2206@gmail.com>
2015-11-22 09:11:10 -0600

    Create maps.js

---
+++ maps.js
@@ -0,0 +1,4 @@
+<script type="text/javascript"
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAg2T1zc4lXqSYzOIv2AgYMzKu0w80wmTI&libraries=visualization&sensor=true">
+  window.alert("initialized");
+</script>
