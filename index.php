36f64b9e9d2fef4988bf22d9a3ca392562d3b845
bilalasd <bilal2206@gmail.com>
2015-11-22 09:09:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ foreach ($data->tweets as $value){
 ?>
 <script type="text/javascript"
   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAg2T1zc4lXqSYzOIv2AgYMzKu0w80wmTI&libraries=visualization&sensor=true">
+  window.alert("initialized");
 </script>
 <?php
 
