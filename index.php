64511bfc6a16af45c71278e77193c8964c6e2447
bilalasd <bilal2206@gmail.com>
2015-11-22 07:31:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,13 @@ $data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a
 foreach ($data->tweets as $value){
   $city = $value->cde->author->location->city;
   $state = $value->cde->author->location->state;
+  $country = $value->cde->author->location->country;
+  if(country == "United States" && $state != NULL && $city != NULL){
   echo "city".$city;
   echo " ";
   echo "state".$state;
   echo "<br>";
+  }
 }
 //----
 
