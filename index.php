8661212fae4236191a8e8c4b6669c3b9ac2d9237
bilalasd <bilal2206@gmail.com>
2015-11-21 23:14:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,6 @@ define('ACCESS_TOKEN', '22417002-sjVPWcCXuobP5fvb4NIQSv3KiKUakWHgFoHBuAsoZ');
 define('ACCESS_TOKEN_SECRET', 'DUmRu3JYS8uygtRUKxcLhCVkbCY96g7cZSh2WQVut6rxg');
  
 $toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
- 
-$query = array(
-  "q" => "#WorldSeries"
-);
- 
-$results = $toa->get('search/tweets', $query);
- 
-foreach ($results->statuses as $result) {
-  echo $result->user->screen_name . ": " . $result->text . "\n";
-}
+
 
 ?>
