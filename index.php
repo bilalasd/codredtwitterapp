e8457a61b16d3aa8ad4e981085f6d3d72cd6b488
bilalasd <bilal2206@gmail.com>
2015-11-21 23:31:19 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ $settings = array(
  
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
  
-/*$requestMethod = "GET";
+$requestMethod = "GET";
  
-$getfield = '?screen_name=iagdotme&count=20';
+/*$getfield = '?screen_name=iagdotme&count=20';
  
 $twitter = new TwitterAPIExchange($settings);
 echo $twitter->setGetfield($getfield)
