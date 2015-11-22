eafe65a95563e9b1258fb0fdd2248c8d217aeb53
bilalasd <bilal2206@gmail.com>
2015-11-22 12:23:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 </html>
 <?php
 
-$data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23dsadsa&size='.$numberparam));
+$data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23'.$hashtagparam.'&size='.$numberparam));
 
 
 $latArr = array();
