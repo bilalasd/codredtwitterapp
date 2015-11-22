6225b8be77386a5cdd7edec233185427a5dd0c9f
bilalasd <bilal2206@gmail.com>
2015-11-22 09:30:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<style type = "text/css">
+  html, body {height: 100%; margin: 0; padding: 0;}
+  #map {height: 100%; }
+  </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script type="text/javascript">
+    var map;
+    function initMap(){
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+        }};
+        }
+        </script>
+        <script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBKmD1B6cH7FzUYeCS0i3G9CMTJ0Rtbd8&callback=initMap">
+    </script>
+    </body>
+    </html>
+/*<?php
 $data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=10'));
 
 
@@ -59,3 +83,4 @@ heatmap.setMap(map);
 <?php
 
 ?>
+*/
