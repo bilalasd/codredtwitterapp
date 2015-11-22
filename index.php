41f94d822fce09841a3bf5b35f9c08af39d507fc
bilalasd <bilal2206@gmail.com>
2015-11-22 01:48:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,27 @@ $settings = array(
     'consumer_secret' => "CUMtdeX91Z0JwpY6Jq0X90aZvwW10MeSiLZ7jEcIAmw2nhMYa2"
 );
  
-$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$url = 'https://api.twitter.com/1.1/blocks/create.json';
+
+$postfields = array(
+    'screen_name' => 'usernameToBlock', 
+    'skip_status' => '1'
+);
+/** Perform the request and echo the response **/
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->buildOauth($url, $requestMethod)
+             ->setPostfields($postfields)
+             ->performRequest();
+             
+ /*
  
-$requestMethod = "GET";
+$requestMethod = 'POST';
  
 $getfield = '?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4';
  
 $twitter = new TwitterAPIExchange($settings);
 echo $twitter->setGetfield($getfield)
              ->buildOauth($url, $requestMethod)
-             ->performRequest();
+             ->performRequest();*/
 
 ?>
