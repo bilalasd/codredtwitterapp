7773ea3078e6385ee9223fd80857d9fa178ac2cc
bilalasd <bilal2206@gmail.com>
2015-11-22 08:17:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=1000'));
+$data = json_decode(file_get_contents('https://e8108483-560b-47ad-8a92-a4b67e43a2fa:SN4DCNwzwT@cdeservice.mybluemix.net/api/v1/messages/search?q=%23asd&size=10'));
 
 
 //for each tweet, get data
